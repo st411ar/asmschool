364e4720e7f890328b72e1fc681bf09a1b3e3a90
Vitaly Kiselev <st411ar@ya.ru>
2017-11-10 22:43:06 +0300

    added kernel building and injecting it to the floppy image

--- part-4/build.sh
+++ part-4/build.sh
@@ -1,3 +1,6 @@
 nasm -f bin -o boot.bin boot.asm
 mkdosfs -C floppy.img 1440
 dd conv=notrunc if=boot.bin of=floppy.img
+
+nasm -f bin -o mykernel.bin mykernel.asm
+mcopy -i floppy.img mykernel.bin ::/
